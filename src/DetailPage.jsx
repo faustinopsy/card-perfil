5f497705fa74b3fcfc35b016e099a8e5a6e204a9
faustinopsy <rodrigohipnose@gmail.com>
2023-12-14 16:34:34 -0300

    modified:   src/App.css 	modified:   src/DetailPage.jsx 	modified:   src/json/home.json 	modified:   src/json/persona.json

--- src/DetailPage.jsx
+++ src/DetailPage.jsx
@@ -2,22 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function DetailPage() {
-  const [details, setDetails] = useState(null);
+  const [detail, setDetail] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`/src/json/details_${id}.json`)
+    fetch('/src/json/home.json') 
       .then(response => response.json())
-      .then(data => setDetails(data));
+      .then(data => {
+        const itemDetail = data.find(item => item.id.toString() === id);
+        setDetail(itemDetail);
+      });
   }, [id]);
 
-  if (!details) {
+  if (!detail) {
     return <div>Carregando...</div>;
   }
 
   return (
-    <div>
-      {/* Renderize os detalhes do card aqui */}
+    <div className="card">
+      <h2>{detail.conteudo}</h2>
     </div>
   );
 }
