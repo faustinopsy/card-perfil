7e99d87db69f23588c00dd709ffd0473dcba06f9
faustinopsy <rodrigohipnose@gmail.com>
2023-12-15 16:57:47 -0300

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
-})
+  base: '/card-perfil/',
+  plugins: [react()]
+});
